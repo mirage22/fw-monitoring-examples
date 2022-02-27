3130c9fae203f6b1ebc755481a4811aeb64895ed
Christian Meyer <code@chrisime.xyz>
2022-02-27 16:37:31 -0300

    docker stuff

--- buildMonitoringContainers.sh
+++ buildMonitoringContainers.sh
@@ -2,4 +2,5 @@
 docker build -f ./DockerfileKTor --no-cache -t ktor-monitoring .
 docker build -f ./DockerfileMicronaut --no-cache -t micronaut-monitoring .
 docker build -f ./DockerfileSpringBoot --no-cache -t spring-boot-monitoring .
+docker build -f ./DockerfileSpringBootKt --no-cache -t spring-boot-monitoring-kt .
 docker build -f ./DockerfileQuarkusJava --no-cache -t quarkus-java-monitoring .

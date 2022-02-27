060027b7a311676f8106d9419e6786da020e716c
Christian Meyer <code@chrisime.xyz>
2022-02-27 16:31:18 -0300

    spring boot kotlin example

---
+++ spring-boot-monitoring-kt/src/main/kotlin/xyz/chrisime/monitoring/spring/boot/service/HalloService.kt
@@ -0,0 +1,8 @@
+package xyz.chrisime.monitoring.spring.boot.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class HalloService {
+    fun hallo() = "Welcome Spring-Boot-Kotlin monitoring example"
+}

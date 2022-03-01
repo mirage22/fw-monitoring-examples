5fd0644140a0741a2037b8782972202ba11117f3
Christian Meyer <code@chrisime.xyz>
2022-02-28 21:17:01 -0300

    explicitly pass platform: arm and intel based

--- buildMonitoringContainers.sh
+++ buildMonitoringContainers.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-docker build -f ./DockerfileKTor --no-cache -t ktor-monitoring .
-docker build -f ./DockerfileMicronaut --no-cache -t micronaut-monitoring .
-docker build -f ./DockerfileSpringBoot --no-cache -t spring-boot-monitoring .
-docker build -f ./DockerfileQuarkusJava --no-cache -t quarkus-java-monitoring .
+ARCH=$(uname -m)
+if [ "$ARCH" = 'arm64' ]; then
+  DOCKER_ARCH='linux/arm64/v8'
+else
+  DOCKER_ARCH='linux/amd64'
+fi
+
+docker build -f ./DockerfileKTor --platform $DOCKER_ARCH --no-cache -t ktor-monitoring .
+docker build -f ./DockerfileMicronaut --platform $DOCKER_ARCH --no-cache -t micronaut-monitoring .
+docker build -f ./DockerfileMicronautKt --platform $DOCKER_ARCH --no-cache -t micronaut-monitoring-kt .
+docker build -f ./DockerfileSpringBoot --platform $DOCKER_ARCH --no-cache -t spring-boot-monitoring .
+docker build -f ./DockerfileQuarkusJava --platform $DOCKER_ARCH --no-cache -t quarkus-java-monitoring .

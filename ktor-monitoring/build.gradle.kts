0d398a3fff0beed964a33a02d96f3f3d68db1e59
Christian Meyer <code@chrisime.xyz>
2022-03-04 15:49:54 -0300

    bumped versions and cleaned up build script

--- ktor-monitoring/build.gradle.kts
+++ ktor-monitoring/build.gradle.kts
@@ -13,9 +13,9 @@ val ktor_micrometer_prometheus_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.5.30"
-    kotlin("plugin.serialization") version "1.5.30"
-    id ("com.github.johnrengelman.shadow") version "7.1.2"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.wengnerits.monitoring.ktor"
@@ -24,7 +24,6 @@ version = "1.0-SNAPSHOT"
 application {
     mainClass.set("com.wengnerits.monitoring.ktor.ApplicationKt")
 }
-
 
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
@@ -42,27 +41,50 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
+
 repositories {
     mavenCentral()
 }
 
-tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        showExceptions = true
-        showCauses = true
-        showStackTraces = true
-        exceptionFormat = TestExceptionFormat.FULL
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = java_version
 
-        events(TestLogEvent.FAILED, TestLogEvent.SKIPPED, TestLogEvent.PASSED)
+            apiVersion = "1.6"
+            languageVersion = "1.6"
 
+            freeCompilerArgs = listOf(
+                    "-Xjsr305=strict",
+                    "-Xstrict-java-nullability-assertions"
+            )
+        }
     }
-}
 
-tasks.compileKotlin{
-    kotlinOptions.jvmTarget = "${java_version}"
-}
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = java_version
+            apiVersion = "1.6"
+            languageVersion = "1.6"
 
-tasks.compileTestKotlin {
-    kotlinOptions.jvmTarget = "${java_version}"
+            freeCompilerArgs = listOf(
+                    "-Xjsr305=strict",
+                    "-Xstrict-java-nullability-assertions"
+            )
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+
+        testLogging {
+            showExceptions = true
+            showCauses = true
+            showStackTraces = true
+            showStandardStreams = true
+            exceptionFormat = TestExceptionFormat.FULL
+
+            events(TestLogEvent.FAILED, TestLogEvent.SKIPPED, TestLogEvent.PASSED)
+        }
+    }
 }

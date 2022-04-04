c22bdacec757fdf3d2c49e75c8abf2822a50249c
Miro Wengner <miro.wengner@gmail.com>
2022-04-04 11:43:42 +0200

    update

--- ktor-monitoring/src/main/kotlin/com/wengnerits/monitoring/ktor/Application.kt
+++ ktor-monitoring/src/main/kotlin/com/wengnerits/monitoring/ktor/Application.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Miroslav Wengner
+ * Copyright (c) 2022, Miroslav Wengner (@miragemiko)
  *
  * fw-monitoring-examples is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
